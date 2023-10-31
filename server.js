102ff6beb7f7fee971f510ab4ae32c02f7f27f77
Lucas Rangel <elheus@gmail.com>
2023-10-30 23:04:11 -0300

    T

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var body = req.body;
+console.log(body);
+res.send('via post');
+
 function soma (a , b){
    return a + b;
 }
@@ -14,6 +21,15 @@ function multiplicar ( a , b ){
    return a * b;
 }
 
+app.use(express.json());
+
+app.post('/api/post-example' , (req, res) => {
+   const data = req.body;
+   console.log('Dados recebidos:', data);
+   res.send('Requisição POST bem-sucedida!');
+ }
+)
+
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
@@ -21,14 +37,10 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
-app.get('/soma/:valorA/:valorB', function (req ,res){
-   const valorA = parseFloat(req.params.valorA);
-   const valorB = parseFloat(req.params.valorB);
-   if(isNaN(valorA) || isNaN(valorB)){
-      res.send("Valores Invalidos");
-   } else {
-      res.send("Valor da soma A + B é : "+ soma(valorA,valorB));
-   }
+app.get('/soma', function (req ,res){
+   var body = req.body;
+   var resultado = soma(body.a, body.b);
+   res.send(`O resultado da soma de ${body.a} e ${body.b} é ${resultado}`);
 })
 
 app.get('/subtracao/:valorA/:valorB', function (req ,res){
